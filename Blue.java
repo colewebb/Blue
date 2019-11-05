b2c03b33c527afdd73c3d88f8ac40004d851e53f
Cole Webb <cole.webb@outlook.com>
2019-11-05 14:40:32 -0700

    blocking stuff out

---
+++ Blue.java
@@ -0,0 +1,50 @@
+public class Blue {
+    // some variables
+    String currentDirectory;
+    String[] directoryHistory;
+
+    // a new dir command
+    protected void dir () {
+
+    }
+    // git push
+    protected void gitPush () {
+
+    }
+    // git rollback
+    protected void gitRollBack () {
+        
+    }
+    // run arbitrary program
+    protected void run (String program) {
+
+    }
+    // run arbitrary program here
+    protected void runHere (String program) {
+        
+    }
+    // run file explorer
+    protected void files () {
+        
+    }
+    // run file explorer here
+    protected void filesHere () {
+
+    }
+    // run web browser of choice
+    protected void web () {
+
+    }
+    // run web browser at selected location
+    protected void webSomewhere (String url) {
+        
+    }
+    // setup Blue in given folders
+    protected void init () {
+        
+    }
+    // help command
+    protected void help () {
+
+    }
+ }
